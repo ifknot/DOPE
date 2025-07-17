fb4f234adcfe7928533c05dab1a8558ac1f1bdd2
ifknot <ifknot@hotmail.com>
2025-07-17 21:43:46 +0100

    dope tape and tests

---
+++ src/DOPE/dope_tape.h
@@ -0,0 +1,11 @@
+#ifndef DOPE_TAPE_H
+#define DOPE_TAPE_H
+
+// dope_constants.h max line size 80 max lines 99
+// dope_types.h
+
+// dope_read_tape (filename line_array_t)
+// dope_read_line(file_handle, char*)
+// dope_wipe_lines(line_array_t*)
+
+#endif
