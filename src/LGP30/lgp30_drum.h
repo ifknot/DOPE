0a33c50947ff7ca17b92bddd653eb1abf19f89c7
ifknot <ifknot@hotmail.com>
2025-07-17 01:18:48 +0100

    Update lgp30_drum.h

--- src/LGP30/lgp30_drum.h
+++ src/LGP30/lgp30_drum.h
@@ -5,12 +5,28 @@
 #include <stdbool.h>
 
 #define LGP30_DRUM_CAPACITY     4096    // 4K of 31 bit words
-#define LGP30_DRUM_DELAY_MS     17      // Drum rotation latency (16.7ms per full rev at 3600 RPM)
+#define LGP30_BITMASK_31        0x7FFFFFFF  // Mask for 31 usable bits (bits 0-30)
+
+// LGP-30 instruction timing approximations
+// From the LGP-30 manual “over 400 additions per second” (Programming Manual, p.7)
+#define LGP30_ADD_RATE    400  // ops/sec
+#define LGP30_ADD_TIME    (1000000 / LGP30_ADD_RATE)  // = 2500µs per add
+#define LGP30_DRUM_DELAY_MS     17      // Drum rotation latency (16.7ms per full rev at 3700 RPM)
 #define LGP30_SECTOR_DELAY_US   4       // ~4µs per sector (4096 sectors / 16.7ms)
+
+// Approximate how many sectors will have passed the read head per op
+#define SECTORS_PER_ADD       (LGP30_ADD_TIME / LGP30_SECTOR_DELAY_US)
+#define SECTORS_PER_MUL       (SECTORS_PER_ADD * 3)  // intuitive guess
+#define SECTORS_PER_DIV       (SECTORS_PER_ADD * 12) // longer for division
+#define SECTORS_PER_IO        (SECTORS_PER_ADD * 2)
 
 typedef int32_t     lgp30_word_t;
 typedef uint16_t    lgp30_size_t;
 
+/*
+ * The LGP-30 word size was 32 bits with the 32nd bit used as a spacer (on the magnetic drum)
+ * and always zero in memory, making for effective 31 bits (0…30)
+ */
 typedef struct {
     lgp30_word_t memory[LGP30_DRUM_CAPACITY];
     lgp30_size_t pc;                // 12-bit program counter
